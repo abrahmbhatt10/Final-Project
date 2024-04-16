510634cb9483b133ead0834312b3d740a0324659
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2024-04-16 15:00:32 -0700

    did that

---
+++ Calculator.java
@@ -0,0 +1,96 @@
+public class Calculator {
+    private double a;
+    private double b;
+    private double x;
+    private int n;
+    private int power;
+    private double c;
+
+    public Calculator(double a, double b, double x, int n, int power, double c) {
+        this.a = a;
+        this.b = b;
+        this.x = x;
+        this.n = n;
+        this.power = power;
+        this.c = c;
+    }
+
+    public String getDerivative(Function f){
+
+    }
+
+    public double getAreaUnderTheCurve(Function f, a, b)
+    {
+
+    }
+
+    public String getAntiDerivative(Function f){
+
+    }
+
+    public String getTaylorPolynomial(Function f)
+    {
+
+    }
+
+    public double getReimannSum()
+    {
+
+    }
+
+    public double getLagrangeRemainderBound()
+    {
+
+    }
+
+
+
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+}
---
+++ Function.java
@@ -0,0 +1,8 @@
+public class Function extends Calculator{
+    private String f;
+
+    public Function(double a, double b, double x, int n, int power, double c, String f) {
+        super(a, b, x, n, power, c);
+        this.f = f;
+    }
+}
